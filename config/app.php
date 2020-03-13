7255956e5f9ce7cf1e0a8f14b2c9101bbc419bb5
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-03-13 15:35:55 +0000

    Instalando o pacote de repositories.

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
 
         /*
          * Application Service Providers...
