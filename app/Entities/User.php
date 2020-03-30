037ddc5aefcf4226925e9d688dc69d6cd48f17da
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-03-30 19:54:41 -0300

    Adicionando attribute password on fillable table Users.

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'telephone', 'additional'
+        'name', 'email', 'password', 'telephone', 'additional'
     ];
 
     /**
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -69,6 +69,7 @@ class UserController extends Controller
 
             $user = $this->userRepository->create($request->all());
 
+
             return (new UserResponse($user))
                 ->response()
                 ->setStatusCode(Response::HTTP_CREATED);
