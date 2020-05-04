8c2a36116cd9a60e4537914fcc993e31c8d903b2
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-05-04 16:16:07 -0300

    Removendo o 'password' do hidden da Enidade User.

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'remember_token',
     ];
 
     /**
