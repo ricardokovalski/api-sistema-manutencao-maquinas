d39da9c3364dffd31415767e64de116c7f85c74e
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-05-06 15:07:59 -0300

    Ajustando tempo do cron e realizando testes.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('check:machines')
-            ->dailyAt('03:00')
+            ->dailyAt('15:15')
             ->withoutOverlapping();
     }
 
