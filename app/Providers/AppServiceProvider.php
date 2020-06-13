42de2321d2a9a0e986251af629f7b5f8071d25a0
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2020-06-13 18:26:35 -0300

    Adicionando uma condição para que force o https quando o ambiente for produção.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
